ea14ac8525de4353634c77ded212d12466e76a44
leonetti <leonetti.tech@gmail.com>
2015-07-04 09:50:29 -0700

    Finished HR Binary Search Tree test cases

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,46 @@
 var BinarySearchTree = function(value){
+	this.value = value;
 };
 
+BinarySearchTree.prototype.insert = function(value){
+	
+	if (value > this.value) {
+		if (this.right === undefined) {
+			this.right = new BinarySearchTree(value);
+		} else {
+			this.right.insert(value);
+		}
+	} else if (value < this.value) {
+		if (this.left === undefined) {
+			this.left = new BinarySearchTree(value);
+		} else {
+			this.left.insert(value);
+		}
+	}
+};
+
+BinarySearchTree.prototype.contains = function(value){
+	if (value === this.value) {
+		return true;
+	}
+	if (value > this.value && this.right !== undefined) {
+		return this.right.contains(value);
+	} else if (value < this.value && this.left !== undefined) {
+		return this.left.contains(this.left);
+	}
+	
+	return false;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb){
+	cb(this.value);
+	if (this.left) {
+		return this.left.depthFirstLog(cb);
+	}
+	if (this.right) {
+		return this.right.depthFirstLog(cb);
+	}
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
