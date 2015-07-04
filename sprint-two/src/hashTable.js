05b305a7b73917c53c2f87286baacf63d3a18db2
leonetti <leonetti.tech@gmail.com>
2015-07-04 09:50:53 -0700

    Finished HR Hash Table tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,16 +5,48 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  
+  var ourArray = [];
+  ourArray.push(k);
+  ourArray.push(v);
+
+  if (this._storage.get(i) === undefined) {
+	  var ourValue = LinkedList();
+	  ourValue.addToTail(ourArray);
+	  this._storage.set(i, ourValue)
+  } else {
+  	  this._storage.get(i).addToTail(ourArray);
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var nodeHead = this._storage.get(i).head;
+  var descendLinkedList = function(node) {
+  	if (k === node.value[0]) {
+  		return node.value[1];
+  	} else if (node.next.value) {
+  		return descendLinkedList(node.next);
+  	}
+  };
+  return descendLinkedList(nodeHead);
 };
 
 HashTable.prototype.remove = function(k){
+	var i = getIndexBelowMaxForKey(k, this._limit);
+	var nodeHead = this._storage.get(i).head;
 
+	var descendLinkedList = function(node) {
+  	if (k === node.value[0]) {
+  		node.value[1] = null;
+  	} else if (node.next) {
+  		descendLinkedList(node.next);
+  	}
+  };
+  descendLinkedList(nodeHead);
 };
+
 
 
 
